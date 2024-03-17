62293a5702d2cb58bdcc862f86aa32d16108c6f6
gyl <1412771048@qq.com>
2024-03-17 22:28:19 +0800

    x

---
+++ build/src/one/CMakeFiles/one.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "../../../lib/libone.a"
+  "../../../lib/libone.pdb"
+  "CMakeFiles/one.dir/1.cpp.o"
+  "CMakeFiles/one.dir/1.cpp.o.d"
+  "CMakeFiles/one.dir/__/main.cpp.o"
+  "CMakeFiles/one.dir/__/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/one.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/src/three/CMakeFiles/three.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "../../../lib/libthree.pdb"
+  "../../../lib/libthree.so"
+  "CMakeFiles/three.dir/3.cpp.o"
+  "CMakeFiles/three.dir/3.cpp.o.d"
+  "CMakeFiles/three.dir/__/main.cpp.o"
+  "CMakeFiles/three.dir/__/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/three.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
